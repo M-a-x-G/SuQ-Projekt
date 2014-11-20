80a240923f2f270166d176f139a7091be6729677
Max Gregor <gregor@fh-brandenburg.de>
2014-11-20 16:47:01 +0100

    add dots

---
+++ src/main/java/de/fhb/suq/dictionary/dto/DefinitionDTO.java
@@ -0,0 +1,10 @@
+package de.fhb.suq.dictionary.dto;
+
+import java.util.List;
+
+/**
+ * Created by Max on 20.11.14.
+ */
+public class DefinitionDTO {
+    private List<List<String>> definitions;
+}
